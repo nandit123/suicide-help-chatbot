a5f5e785dec1e23d4f244f6ad229747ef53ab7b2
Divyanshu <divyanshu@quaqua.com>
2020-06-22 00:14:28 +0530

    console.log statement to check attachment received

--- index.js
+++ index.js
@@ -77,6 +77,7 @@ app.post('/webhook', (req, res) => {
 		}}else if (webhook_event.postback) {
 			handlePostback(sender_psid, webhook_event.postback);
 		} else if (webhook_event.message.attachments) {
+			console.log("attachment received");
 			handleMessage (sender_psid, webhook_event.message);
 		}
       });
