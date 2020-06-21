26de0cafd36275f60745ef15d47bdb56e7a38e88
Divyanshu <divyanshu@quaqua.com>
2020-06-22 02:04:15 +0530

    console.log printing find statement

--- index.js
+++ index.js
@@ -40,8 +40,7 @@ app.post('/webhook', (req, res) => {
 		client.connect(err => {
 		const collection = client.db("db1").collection("user_data");
 		var query = {user_id: sender_psid};
-		var x = collection.find(query);
-		console.log('asdasdsa', x);
+		collection.find(query);
 		// perform actions on the collection object
 		client.close();
 		});
