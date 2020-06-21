3f25d0f73a63957507cd2c7a97a54207227ad96d
Divyanshu <divyanshu@quaqua.com>
2020-06-21 23:35:31 +0530

    Added callSendAPI trigger in task1

--- index.js
+++ index.js
@@ -283,12 +283,20 @@ function handlePostback(sender_psid, received_postback) {
         "payload": {
           "template_type": "generic",
           "elements": [{
-            "title": "Task 1",
+            "title": "TASK 1",
+			"image_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fimage&psig=AOvVaw1LtELrRsk3MVcvmv_jgUz-&ust=1592847818691000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOjUmta6k-oCFQAAAAAdAAAAABAD",
             "subtitle": "Tap a button to answer.",
+            "default_action": {
+				"type": "web_url",
+				"url": "https://www.youtube.com/watch?v=wFEvwxi8AOw",
+				"messenger_extensions": "true",
+				"webview_height_ratio": "tall"
+			  },
           }]
         }
       }
     }
+	callSendAPI(sender_psid, response);
   }
 }
 
