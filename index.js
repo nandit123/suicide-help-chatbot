0fcf7c81f205ff044e65d242996223d41c911772
Divyanshu <divyanshu@quaqua.com>
2020-06-24 19:02:28 +0530

    quickreply for phone call

--- index.js
+++ index.js
@@ -330,12 +330,22 @@ function handlePostback(sender_psid, received_postback) {
 		  "object": "page",
 		  "entry": [
 			{
+			  "id": "<PAGE_ID>",
+			  "time": 1502905976963,
 			  "messaging": [
 				{
+				  "sender": {
+					"id": "1254459154682919"
+				  },
+				  "recipient": {
+					"id": "682498171943165"
+				  },
+				  "timestamp": 1502905976377,
 				  "message": {
 					"quick_reply": {
-					  "payload": "phoneCall"
+					  "payload": "<PHONE_NUMBER>"
 					},
+					"mid": "m_AG5Hz2Uq7tuwNEhXfYYKj8mJEM_QPpz5jdCK48PnKAjSdjfipqxqMvK8ma6AC8fplwlqLP_5cgXIbu7I3rBN0P",
 					"text": "1234"
 				  }
 				}
