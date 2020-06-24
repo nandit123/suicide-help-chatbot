ea4237aaa34d72aa53b2ed1879ef621eae96029e
Nandit Mehra <nandit.mehra.15csc@bml.edu.in>
2020-06-24 13:40:12 +0530

    console.log webhook event so that we can get the one time notification details

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post('/webhook', (req, res) => {
       // Gets the message. entry.messaging is an array, but 
       // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
-
+      console.log('webhook event: ', webhook_event);
       // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
       console.log('Sender PSID: ' + sender_psid);
